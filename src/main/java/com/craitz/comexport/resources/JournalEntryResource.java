1543586e49f63498b36441d8795508682f942304
Camilo Raitz da Silva <camilo.rsilva@hsl.org.br>
2018-12-07 14:38:26 -0200

    busca de lançamento contábil

--- src/main/java/com/craitz/comexport/resources/JournalEntryResource.java
+++ src/main/java/com/craitz/comexport/resources/JournalEntryResource.java
@@ -1,9 +1,11 @@
 package com.craitz.comexport.resources;
 
 import java.util.List;
+import java.util.Optional;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
@@ -13,20 +15,25 @@ import com.craitz.comexport.domains.JournalEntry;
 import com.craitz.comexport.repositories.JournalEntryRepository;
 
 @RestController
-@RequestMapping("/journalEntries")
+@RequestMapping("/journal-entries")
 public class JournalEntryResource {
 	
 	@Autowired
 	private JournalEntryRepository journalEntryRepository;
 
 	@GetMapping()
-	public List<JournalEntry> list() {
+	public List<JournalEntry> getAllEntries() {
 		return journalEntryRepository.findAll();
 	}
 	
 	@PostMapping()
-	public void save(@RequestBody JournalEntry journalEntry) {
+	public void insertEntry(@RequestBody JournalEntry journalEntry) {
 		journalEntryRepository.save(journalEntry);
+	}
+	
+	@GetMapping(value = "/{id}")
+	public JournalEntry findEntry(@PathVariable("id") Long id) {
+		JournalEntry entry = journalEntryRepository.findById(id).get();
 	}
 }
 	
